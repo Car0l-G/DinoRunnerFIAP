134cb099d0c5410f3027f5c8845b2c57747de709
rodrigocruzdomingos <153248177+rodrigocruzdomingos@users.noreply.github.com>
2026-09-17 22:24:34 -0300

    Start do cactus

--- DinoRunner/Assets/Scripts/cactus.cs
+++ DinoRunner/Assets/Scripts/cactus.cs
@@ -4,7 +4,14 @@ public class cactus : MonoBehaviour
 {
     [SerializeField] private float speed = 10.0f;
     [SerializeField] private float positionXtoDestroy = -12.0f;
-    
+
+
+    private void Start()
+    {
+        Debug.Log("Teste...");
+    }
+
+
     void Update()
     {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
